62f2b816c67bbaf09c8df8e06a05cee65fe029f3
Aljoscha Zöller <mail@aljoschazoeller.com>
2023-06-22 15:39:25 +0200

    fetch with swr

--- pages/characters/1.js
+++ pages/characters/1.js
@@ -1,17 +1,24 @@
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useSWRConfig } from "swr";
 
 export default function Character() {
   const id = 1;
+  const URL = `https://swapi.dev/api/people/${id}`;
+  const { data, error, isLoading } = useSWR(URL);
+
+  if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>Waiting for spaceship to arrive... 🛸</div>;
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
       />
     </Layout>
   );
